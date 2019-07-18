14bc24195fa2e38949d2c345de191611a7efa3e3
Danny Town <47186976+dannyotown@users.noreply.github.com>
2019-07-17 20:17:18 -0500

    fade in cover

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 
 
 function openMenu(){
-        $('.collapseBox').animate({height:'400'});
+        $('.collapseBox').animate({height:'800'});
         $('.openButton').css('display','none');
         $('.closeButton').css('display','block');
+        $( ".collapseFlex" ).fadeIn(10);
 }
 function closeMenu(){
         $('.collapseBox').animate({height:'65'});
         $('.closeButton').css('display','none');
         $('.openButton').css('display','block');
+        $( ".collapseFlex" ).css('display','none');
+
+}
+function weBack(){
+    $('.inputSelect').css('display','block');
 }
